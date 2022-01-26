1a67baf1e0a65cfac7a68ed7eab5a0e7e5ee4534
prakaimook <98022722+prakaimook@users.noreply.github.com>
2022-01-26 17:10:00 +0700

    test

--- Ex4-1.py
+++ Ex4-1.py
@@ -1,1 +1,8 @@
-print(1)
+FE = 100
+GB = 100
+ICS = 100
+CP = 100
+print('Foundation English:', FE)
+print("General Business:", GB)
+print("Introduction to Computer Systems:", ICS)
+print("Computer Programming:", CP)
