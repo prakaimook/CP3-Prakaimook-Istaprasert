9b25e3e6cfe649462312f8354de4ff814ab40240
prakaimook <98022722+prakaimook@users.noreply.github.com>
2022-02-10 23:18:23 +0700

    Rename

---
+++ Lecture72_List in List.py
@@ -0,0 +1,28 @@
+#Declare list
+menuList = []
+priceList = []
+amountList = []
+
+#Loop for gettin input info
+while True:
+    menuName = input("Menu name : ")
+    if(menuName.lower() == "total"):
+        break
+    else:
+        menuPrice = int(input("Price : "))
+        menuAmount = int(input("Amount : "))
+        menuList.append([menuName ,menuPrice ,menuAmount])
+
+# Create function showBill()
+def showBill():
+    print("-------- My Shop ---------")
+    totalPrice = 0
+    for i in range(len(menuList)):
+        menuTotal = menuList[i][1] * menuList[i][2]
+        print(menuList[i][0],menuList[i][1], "Baht each" , "Sub total" ,menuTotal,"Baht")
+        totalPrice = totalPrice + menuTotal
+    print("Total", totalPrice, "บาท")
+
+showBill()
+
+
