70a42beb5630efd38e39356e8c53989a29779be3
prakaimook <98022722+prakaimook@users.noreply.github.com>
2022-02-15 01:42:18 +0700

    Final

--- Ex21_GUIprograming.py
+++ Ex21_GUIprograming.py
@@ -2,32 +2,37 @@ from tkinter import *
 import math
 
 def leftClickButton(event):
-    result = float(textBoxWeight.get())/math.pow(float(textBoxHeight.get())/100,2)
-    if result >= 30:
-        labelResult.configure(text="อ้วนมาก")
-    elif result >=25:
-        labelResult.configure(text="อ้วน")
-    elif result >=23:
-        labelResult.configure(text="น้ำหนักเกิน")
-    elif result >=18.6:
-        labelResult.configure(text="น้ำหนักปกติ")
+    bmi = float(textBoxWeight.get())/math.pow(float(textBoxHeight.get())/100,2)
+
+    if bmi >= 30:
+        result = "อ้วนมาก"
+    elif bmi >=25:
+        result="อ้วน"
+    elif bmi >=23:
+        result="น้ำหนักเกิน"
+    elif bmi >=18.6:
+        result="น้ำหนักปกติ"
     else:
-        labelResult.configure(text="ผอมเกินไป")
+        result="ผอมเกินไป"
+    labelResult.configure(text=bmi)
+    labelResult2.configure(text=result)
 
 MainWindow = Tk()
-labelHeight = Label(MainWindow, text="ส่วนสูง (cm.)")
+labelHeight = Label(MainWindow, text="ส่วนสูง (cm)")
 labelHeight.grid(row=0,column=0)
 textBoxHeight = Entry(MainWindow)
 textBoxHeight.grid(row=0,column=1)
-labelWeight =  Label(MainWindow, text="น้ำหนัก")
+labelWeight =  Label(MainWindow, text="น้ำหนัก (kb)")
 labelWeight.grid(row=1,column=0)
 textBoxWeight = Entry(MainWindow)
 textBoxWeight.grid(row=1,column=1)
 calculateButton = Button(MainWindow,text="คำนวน")
 calculateButton.grid(row=2, column=-0)
 calculateButton.bind('<Button-1>', leftClickButton)
-labelResult = Label(MainWindow,text="ผลลัพธ์")
+labelResult = Label(MainWindow,text="BMI")
 labelResult.grid(row=2,column=1)
+labelResult2 = Label(MainWindow,text="ผลลัพธ์")
+labelResult2.grid(row=3,column=1)
 
 MainWindow.mainloop()
 
